7b3953fe5783f893502f10459e12875f3dc3f688
Phillip Caudell <phillipcaudell@gmail.com>
2012-08-24 16:20:52 +0100

    Removed PCLog from files.

--- PCSingleRequestLocationManager.m
+++ PCSingleRequestLocationManager.m
@@ -1,17 +1,9 @@
 //
-//    Copyright 2012 Phillip Caudell
+//  PCWebServiceLocationManager.m
+//  Glenigan
 //
-//    Licensed under the Apache License, Version 2.0 (the "License");
-//    you may not use this file except in compliance with the License.
-//    You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-//    Unless required by applicable law or agreed to in writing, software
-//    distributed under the License is distributed on an "AS IS" BASIS,
-//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//    See the License for the specific language governing permissions and
-//    limitations under the License.
+//  Created by Phillip Caudell on 23/08/2012.
+//  Copyright (c) 2012 madebyphill.co.uk. All rights reserved.
 //
 
 #import "PCSingleRequestLocationManager.h"
@@ -78,9 +70,9 @@
 {
     // Debug info
     if (kPCWebServiceLocationManagerDebug) {
-        PCLog(@"PCWebServiceLocationManager: New location: %@", newLocation);
-        PCLog(@"PCWebServiceLocationManager: Horizontal accuracy: %f", newLocation.horizontalAccuracy);
-        PCLog(@"PCWebServiceLocationManager: Vertical accuracy: %f", newLocation.verticalAccuracy);
+        NSLog(@"PCWebServiceLocationManager: New location: %@", newLocation);
+        NSLog(@"PCWebServiceLocationManager: Horizontal accuracy: %f", newLocation.horizontalAccuracy);
+        NSLog(@"PCWebServiceLocationManager: Vertical accuracy: %f", newLocation.verticalAccuracy);
     }
 
     // If accuracy greater than 100 meters, it's probably crap
